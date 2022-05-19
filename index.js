f211abed8303ed2ca3426267f0d1909ef229c6f4
Franz Cortez <franz.cortez@alumnos.ucn.cl>
2022-05-19 10:29:28 -0400

    feat: add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
 import publicRoutes from "./routes/publicRoutes.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
@@ -11,6 +12,19 @@ dotenv.config();
 
 conectarDB();
 
+const dominiosPermitidos = [process.env.FRONTEND_URL];
+
+const corsOption = {
+    origin: function(origin, callback){
+        if(dominiosPermitidos.indexOf(origin) !== -1){
+            callback(null,true);
+        } else {
+            callback(new Error("No Permitido por CORS"));
+        }
+    }
+}
+
+app.use(cors(corsOption));
 
 app.use('/api/public', publicRoutes);
 app.use('/api/usuario', usuarioRoutes);
